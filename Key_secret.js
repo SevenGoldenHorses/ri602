13a153af622eaefcfc6274d1af3f0eb2636fdc28
MisterX <you@example.com>
2018-04-17 19:11:58 +0200

    cambiato Token

--- Key_secret.js
+++ Key_secret.js
@@ -1,8 +1,7 @@
 module.exports = {
   consumer_key:         'AF8aJAkKrTQvbZNz2dD3OPCe3',
   consumer_secret:      'FTgxCURcwU9PIiBnAUUr4byHCNMInm704ymKZp2R4KTvEy2YqY',
-  access_token:         '3089427630-dFl2ww3NhMrZuK9gMfNX6Jun58ZAFuEFx3xYaaF',
-  access_token_secret:  'cZWpSkzlDDdrfOcm4T1QbNlaLD1H5rvgaDVPF1w3cMsoX',
+  access_token:         '3089427630-bSYHy7PB5teWXsOQmDbJehdELxG7T5aoZq7PBnA',
+  access_token_secret:  'E84txaAWasa1eKu3RnyB8geYDmZJkoqKAuk06QVp9xbHj',
   // timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
 }
-
